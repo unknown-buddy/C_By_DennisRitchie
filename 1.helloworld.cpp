255c4e01a400b5890cbd2bde4fbbc2cb856f6432
unknown-buddy <45201444+unknown-buddy@users.noreply.github.com>
2018-11-20 21:40:19 +0530

    1.HelloWorld
    Basic c program with the helloworld

---
+++ 1.helloworld.cpp
@@ -0,0 +1,14 @@
+/*
+ In this course we are going to see programming in c with the
+reference to the book by dennis Ritchie,Hope you guys enjoy!!
+*/
+
+#include<stdio.h>//header file which contains basic I/O standards like printf,scanf
+main()//main block of code which will be executed first and is also a fuction without parameter
+{
+	printf("Hello,world\n");/*printf is a function eith its string constant as a parameter
+	here sequence of characters with double quotes is known as string constants.
+	 \n - which is an enscape sequence character is known as newline by default
+	 printf doesn't make any new line as in java which println. 
+	*/
+}
