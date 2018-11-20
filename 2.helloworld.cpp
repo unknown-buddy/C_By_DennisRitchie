48f9522e32b23610f61eb90a87354ee6033828c7
unknown-buddy <45201444+unknown-buddy@users.noreply.github.com>
2018-11-20 21:47:43 +0530

    2.helloworld(modified)
    To explain about the new linefeed escape sequence in the printf system call

---
+++ 2.helloworld.cpp
@@ -0,0 +1,11 @@
+/*
+ As said in the previous code that printf doen't include any new line unless we specify it
+so,in this code there are two printf statements without any \n feed which results 
+in the output on the same line.
+*/
+#include<stdio.h>
+main()
+{
+	printf("hello");
+	printf(",world");
+}
